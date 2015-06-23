1bed2f0b7e3dfc723d757add5294b921cba82816
Martin Polak <brko11@gmail.com>
2015-06-23 12:05:41 +0200

    uprava predavani dat kosiku - vraceni (rozhodilo mi to ceny ve vyhledavani???)

--- app/app_controller.php
+++ app/app_controller.php
@@ -84,13 +84,6 @@
 
 			// administrator ma session timeout 720 sekund * 10 = 2 hodiny
 			Configure::write('Session.timeout', 720);
-
-		} else {
-			// data o kosiku
-			// musim je posilat uz v beforeFilter, protoze se mi tady inicializuje kosik (v beforeRender uz by mohlo byt pozde)
-			App::import('Model', 'CartsProduct');
-			$this->CartsProduct = &new CartsProduct;
-			$this->set('carts_stats', $this->CartsProduct->getStats($this->CartsProduct->Cart->get_id()));
 		}
 		
 		App::import('Model', 'Setting');
@@ -114,6 +107,9 @@
 			if (isset($this->viewVars['opened_category_id'])) {
 				$opened_category_id = $this->viewVars['opened_category_id'];
 			}
+			
+			// data o kosiku
+			$this->set('carts_stats', $this->Product->CartsProduct->getStats($this->Product->CartsProduct->Cart->get_id()));
 			
 			// menu hlavni kategorie
 			$this->set('categories_menu', $this->Product->CategoriesProduct->Category->getSubcategoriesMenuList($opened_category_id, $this->Session->check('Customer')));
@@ -196,7 +192,7 @@
 			$object = null;
 		}
 		$assoc = null;
-	
+
 		if (is_string($object)) {
 			$assoc = null;
 			if (strpos($object, '.')  !== false) {
